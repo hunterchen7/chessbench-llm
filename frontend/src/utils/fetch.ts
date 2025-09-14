0b0a26fcecf73459435625430a42dfda5230f560
Hunter Chen <hello@hunterchen.ca>
2025-09-14 04:18:22 -0400

    udpate config

--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -1,6 +1,9 @@
-const BASE_URL = 'https://worker.hunter-chen7.workers.dev/api/chessbench';
+const BASE_URL = 'https://chessbench.hunterchen.workers.dev/api/chessbench';
 
-export const fetchWithPrefix = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
+export const fetchWithPrefix = async (
+  input: RequestInfo | URL,
+  init?: RequestInit,
+): Promise<Response> => {
   const response = await fetch(BASE_URL + input, init);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
